3ea3a60d33e5ad4b48fc36b9f0555e363fb7e713
Thejaswini Shreekantha <thejaswini.shreekantha@bto.org>
2025-11-13 14:38:51 +0000

    2 Add missing function docstring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,9 +1,21 @@
+""" Performs conversions between Farhenheit, Celsius and Kelvin"""
 SHIFT = 3
 COMMENT = '#'
 climate_data = open('data/sc_climate_data_10.csv', 'r')
 
 
 def FahrToCelsius(fahr):
+    """COnverts fahrenehit to Celsius
+
+    Args:
+        fahr (float): temperature in fahrenheit
+
+    Returns:
+        float: temperature in Celsius
+    """
+    celsius = ((fahr - 32) * (5/9)) 
+    return celsius
+def FahrToKelvin(fahr):
     """COnverts fahrenehit to Kelvin
 
     Args:
@@ -12,9 +24,6 @@ def FahrToCelsius(fahr):
     Returns:
         float: temperature in Kelvin
     """
-    celsius = ((fahr - 32) * (5/9)) 
-    return celsius
-def FahrToKelvin(fahr):
     kelvin = FahrToCelsius(fahr) + 273.15
     return kelvin
 
